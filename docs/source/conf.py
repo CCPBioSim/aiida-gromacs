87c76a255308b74ce2276778ddaf6429ed0bb1ea
James Gebbie-Rayet <james.gebbie@stfc.ac.uk>
2026-08-28 16:08:19 +0100

    Fix regex syntax in nitpick_ignore_regex

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -256,8 +256,8 @@ nitpick_ignore = [
 
 nitpick_ignore_regex = [
     (
-        r'py:class',
-        r'.*\.(AttributesModel|AttributesWriteModel|ReadModel|WriteModel|ConstructorModel|Model)'
+        r"py:class",
+        r".*\.(AttributesModel|AttributesWriteModel|ReadModel|WriteModel|ConstructorModel|Model)",
     ),
 ]
 
